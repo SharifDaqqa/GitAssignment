dae15aa8320461a7bb07111df06f2bbdc8ec660f
SharifDaqqa <Sharifdaqqa02@gmail.com>
2023-05-24 21:21:48 +0300

    update class.java

--- MyClass.java
+++ MyClass.java
@@ -1,10 +1,10 @@
 public class MyClass {
-    int a,b,c;
-    MyClass(int a,int b, int c)
+    int a,b,d;
+    MyClass(int a,int b, int d)
     {
         this.a = a;
         this.b = b;
-        this.c = c;
+        this.d = d;
     }
 
 }
